ebd3addd711778f84a285bbe25f181d66b532ac6
Radu Crisan <radu.crisan@gmail.com>
2016-10-28 11:54:46 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res) {
+   res.send('welcome to my API')
+});
+
+app.listen(port, function() {
+    console.log('running on my port ' + port);
+});
