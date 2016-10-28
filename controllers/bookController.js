d19414830e8b994f7633ba9fdd5adb9474dd4fcc
Radu Crisan <radu.crisan@gmail.com>
2016-10-28 17:12:54 +0300

    added hypermedia

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,9 +1,9 @@
-var bookController = function(Book) {
+var bookController = function (Book) {
 
     var post = function (req, res) {
         var book = new Book(req.body);
         book.save();
-        if(!req.body.title) {
+        if (!req.body.title) {
             res.status(400);
             res.send('Title is required');
         }
@@ -22,7 +22,14 @@ var bookController = function(Book) {
             if (err) {
                 res.status(500).send(err);
             } else {
-                res.json(books);
+                var returnBooks = [];
+                books.forEach(function (element, index, array) {
+                    var newBook = element.toJSON();
+                    newBook.links = {};
+                    newBook.links.self = 'http://'+ req.headers.host + '/api/books/' + newBook._id;
+                    returnBooks.push(newBook);
+                });
+                res.json(returnBooks);
             }
         });
     };
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -23,7 +23,11 @@ var routes = function (Book) {
     });
     bookRouter.route('/:bookId')
         .get(function (req, res) {
-            res.json(req.book);
+            var returnBook = req.book.toJSON();
+            returnBook.links = {};
+            var newLink = 'http://'+ req.headers.host + '/api/books/?gender=' + returnBook.gender;
+            returnBook.links.FilterByGender = newLink.replace(' ', '%20');
+            res.json(returnBook);
         })
         .put(function (req, res) {
             req.book.title = req.body.title;
