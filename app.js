c91669e09eee50aa4b50f75335f4cfe4e5972206
Radu Crisan <radu.crisan@gmail.com>
2016-10-28 13:25:02 +0300

    basic mongodb filtering

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 var express = require('express');
+var mongoose = require('mongoose');
 
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+var Book = require('./models/bookModel');
 var app = express();
 var port = process.env.PORT || 3000;
+var bookRouter = express.Router();
 
-app.get('/', function(req, res) {
-   res.send('welcome to my API')
-});
+app.use('/api', bookRouter);
+bookRouter.route('/books')
+    .get(function(req, res) {
+        var query = {};
+        if(req.query.gender) {
+            query.gender = req.query.gender;
+        }
+        Book.find(query, function(err, books) {
+            if(err) {
+                res.status(500).send(err);
+            } else {
+                res.json(books);
+            }
+        });
+    });
+
 
 app.listen(port, function() {
     console.log('running on my port ' + port);
